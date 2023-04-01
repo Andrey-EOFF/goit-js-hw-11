66501542879c7ba88747874f928dfec2a200110a
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-04-02 02:41:19 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
+const Mustache = require('mustache');
+import Notiflix from 'notiflix';
+import photoCardTemplate from '../src/template/markup-template';
+import PhotoApiService from '../src/template/photo-api-service';
 
-fetch('https://pixabay.com/api/?key=34809960-e72b1bf02b7f952b124a41dc8&q=dog')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.log(error));
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-  
+const photoCards = document.querySelector('.gallery');
+const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+const photoApiService = new PhotoApiService();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onloadMore);
+
+function onSearch(evt) {
+  evt.preventDefault();
+  clearPhotoCards();
+  photoApiService.query = evt.currentTarget.elements.searchQuery.value;
+  if (photoApiService.query === '') {
+    return Notiflix.Notify.warning('Enter text in the search bar please');
+  }
+  loadMoreBtn.style.display = 'block';
+  photoApiService.resetPage();
+  photoApiService.fetchPhotoCards().then(appendPhotoHits);
+}
+
+function onloadMore() {
+  photoApiService.fetchPhotoCards().then(appendPhotoHits);
+}
+
+function appendPhotoHits(data) {
+  const renderedCard = Mustache.render(photoCardTemplate, data);
+  photoCards.insertAdjacentHTML('beforeend', renderedCard);
+
+  // var lightbox = new SimpleLightbox('.gallery img', {
+  //   caption: true,
+  //   captionType: 'attr',
+  //   captionsData: 'alt',
+  //   captionPosition: 'bottom',
+  //   swipeClose: true,
+  //   animationSpeed: 300,
+  // });
+}
+
+function clearPhotoCards() {
+  photoCards.innerHTML = '';
+}
